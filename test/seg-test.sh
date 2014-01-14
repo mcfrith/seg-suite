f584c3926a665f2463c021846520c0b0dd193f96
Martin C. Frith <mcfrith@gmail.com>
2014-01-14 18:54:54 +0900

    Added tests for seg-import UTR and -p options

--- test/seg-test.sh
+++ test/seg-test.sh
@@ -18,9 +18,11 @@ PATH=..:$PATH
     try seg-import genePred hg19refGene.txt
     try seg-import -c genepred hg19refGene.txt
     try seg-import -i genepred hg19refGene.txt
+    try seg-import -p genepred hg19refGene.txt
 
     try seg-import gtf sp.gtf
     try seg-import -c gtf sp.gtf
     try seg-import -i gtf sp.gtf
+    try seg-import -5 -3 gtf sp.gtf
 } |
 diff -u seg-test.txt -

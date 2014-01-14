574933e1107a373e89e69a32df8034dc357ef8c0
Martin C. Frith <mcfrith@gmail.com>
2014-01-14 18:12:29 +0900

    Added tests for seg-import gtf

--- test/seg-test.sh
+++ test/seg-test.sh
@@ -14,8 +14,13 @@ PATH=..:$PATH
 
 {
     try seg-import -h
+
     try seg-import genePred hg19refGene.txt
     try seg-import -c genepred hg19refGene.txt
     try seg-import -i genepred hg19refGene.txt
+
+    try seg-import gtf sp.gtf
+    try seg-import -c gtf sp.gtf
+    try seg-import -i gtf sp.gtf
 } |
 diff -u seg-test.txt -
